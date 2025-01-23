c02c7f6d6389f15818ec7ca8a6f9c43ee065813a
GaganPreet Singh <dmgaganpreet@gmail.com>
2025-01-23 14:02:17 +0530

    fix: wrapped component in suspense

--- app/events/page.js
+++ app/events/page.js
@@ -1,36 +1,35 @@
 'use client'
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import EventCard from "@/components/EventCard";
 import { useSearchParams } from "next/navigation";
 
-function EventPage(){
-
+function EventPage() {
   const searchParams = useSearchParams();
-  const tagQuery = searchParams.get('tag')
-  const artistQuery = searchParams.get('artist')
-  const [events, setEvents] = useState([])
-
+  const tagQuery = searchParams.get('tag');
+  const artistQuery = searchParams.get('artist');
+  const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    const fetchEvents = async() => {
+    const fetchEvents = async () => {
       const data = await fetch("https://qevent-backend.labs.crio.do/events");
       const eventData = await data.json();
 
-      let filteredEvents = []
+      let filteredEvents = [];
 
-      if(tagQuery){
-        filteredEvents = eventData.filter(event =>  event.tags && event.tags.includes(tagQuery))
-      }else if(artistQuery){
-        filteredEvents = eventData.filter(event => event.artist && event.artist.toLowerCase() === artistQuery.toLowerCase())
-      }else {
-        filteredEvents = eventData
+      if (tagQuery) {
+        filteredEvents = eventData.filter(event => event.tags && event.tags.includes(tagQuery));
+      } else if (artistQuery) {
+        filteredEvents = eventData.filter(event => event.artist && event.artist.toLowerCase() === artistQuery.toLowerCase());
+      } else {
+        filteredEvents = eventData;
       }
-      
-      setEvents(filteredEvents)
-    }
+
+      setEvents(filteredEvents);
+    };
+
     fetchEvents();
-  },[])
+  }, [tagQuery, artistQuery]); 
 
   return (
     <div className="h-full w-full flex-wrap flex items-center justify-around mt-8 mb-32">
@@ -41,4 +40,10 @@ function EventPage(){
   );
 }
 
-export default EventPage;
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <EventPage />
+    </Suspense>
+  );
+}
